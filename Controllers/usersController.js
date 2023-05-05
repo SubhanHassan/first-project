6b8d2b1e34cb8d428e28703f951b7493a2726152
SubhanHassan <hassansubhan80@gmail.com>
2023-05-05 17:08:37 +0500

    login and get users  by id and delete

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -4,6 +4,21 @@ const addUser = (body) => {
     return UsersModel.create(body);
 };
 
-module.exports = {
-    addUser
+const getAllUsers = (filter) => {
+    return UsersModel.find(filter).sort({ _id: -1 });
+};
+
+
+const getUserById =() => {
+    return UsersModel.findById(id);
 }
+const updateUser = (id,body) => {
+    const query = {_id: id };
+    return UsersModel.findByIdAndUpdate(query,body,{new: true});
+};
+module.exports = {
+    addUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -3,84 +3,100 @@ var router = express.Router();
 const { UsersController } = require('../Controllers');
 
 /* GET users listing. */
-const users =[
-  {
-  name:'Abc',
-  age:12,
-  username:'username_1',
-  password:'abc123',
-},
-{
-  name:'Abc',
-  age:12,
-  username:'username_2',
-  password:'abc123',
+// const users =[
+//   {
+//   name:'Abc',
+//   age:12,
+//   username:'username_1',
+//   password:'abc123',
+// },
+// {
+//   name:'Abc',
+//   age:12,
+//   username:'username_2',
+//   password:'abc123',
 
-},
-{
-  name:'Abc',
-  age:12,
-  username:'username_3',
-  password:'abc123',
+// },
+// {
+//   name:'Abc',
+//   age:12,
+//   username:'username_3',
+//   password:'abc123',
 
-},
-{
-  name:'Abc',
-  age:12,
-  username:'username_4',
-  password:'abc123',
+// },
+// {
+//   name:'Abc',
+//   age:12,
+//   username:'username_4',
+//   password:'abc123',
 
-},
-{
-  name:'Abc',
-  age:12,
-  username:'username_5',
-  password:'abc123',
+// },
+// {
+//   name:'Abc',
+//   age:12,
+//   username:'username_5',
+//   password:'abc123',
 
-}
-];
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({
-    message:'Successfully festch users',
-    data:users
-});
-});
-router.post('/', async function(req,res,next){
-  const data = req.body;
-  try{
-   const user = await UsersController.addUser(data);
-    return res.send({
-      message:'Successfully saved users',
-      data:user
-  });
-  }catch(error){
-    res.status(200).send({
-      message:'Successfully saved users',
-      data:user
-
-  });
-  
-  res.status(500).send({
-    message:'Something Went Wrong',
-    data:error
-});
-  }
-});
-router.put('/:id',function(req,res,next){
-  const id = req.params.id;
-  const data = req.body;
-  if(!users[id]){
-    return req.send({
-      message: 'This is Invalid',
-      data:null
+// }
+// ];
+/* GET Users Listing. */
+router.get('/', async function(req, res, next) {
+  const filter = req.query;
+  try {
+    const results = await UsersController.getAllUsers(filter);
+    return res.status(200).send({
+      message: 'Successfully fetched users',
+      data: results
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: 'Something went wrong',
+      data: error
     });
   }
-    users[id]=data;
-    res.send({
-    message:'Successfully update users',
-    data:users
 });
+
+  /* Add Users . */  
+
+router.post('/', async function(req,res,next){
+  const data = req.body;
+  try {
+    const user = await UsersController.addUser(data);
+    res.status(200).send({
+      message: 'Successfully saved user',
+      data: user
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: 'Something went wrong',
+      data: error
+    });
+  }
+});
+
+
+
+
+
+
+/* update Users Listing. */
+router.put('/:id',async function(req,res,next){
+  const id = req.params.id;
+  const data = req.body;
+  try {
+    const result = await UsersController.updateUsers(id,data);
+    res.status(200).send({
+      message:'Successfully festch users',
+      data:result
+  });
+
+}catch (error) {
+    res.status(500).send({
+      message:'Something went wrong',
+      data:error
+    
+    });
+}
 });
 router.delete('/:id',function(req,res,next){
    const id =req.params.id;
